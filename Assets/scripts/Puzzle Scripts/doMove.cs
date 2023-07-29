5825bd030d7bb9580a55fc1ea3cafe0cf96a1427
Hussein <husseincak@hotmail.com>
2023-07-29 21:07:29 +0200

    Update doMove.cs

--- Assets/scripts/Puzzle Scripts/doMove.cs
+++ Assets/scripts/Puzzle Scripts/doMove.cs
@@ -6,15 +6,16 @@ public class doMove : BaseAction
 {
     private int direction = 1;
     private bool isActive = false;
-
+    public float down = 1f;
+    public float up = 1f;
 
     void Update()
     {
         if (isActive){
-            if (transform.position.y < -2f){
+            if (transform.position.y < -down){
                 direction = 1;
                 Debug.Log("is moving down!");
-            }else if (transform.position.y > 2f){
+            }else if (transform.position.y > up){
                 direction = -1;
                 Debug.Log("is moving up!");
             }
