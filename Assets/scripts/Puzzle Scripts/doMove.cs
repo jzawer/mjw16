19a63ac3c34569c7a78d9faa9af364ce255096f3
javi <zawer>
2023-07-29 22:33:32 +0200

    add puzzle2 level

--- Assets/scripts/Puzzle Scripts/doMove.cs
+++ Assets/scripts/Puzzle Scripts/doMove.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class doMove : BaseAction
 {
     private int direction = 1;
     private bool isActive = false;
+    private bool isMoving = false;
     public float down = 1f;
     public float up = 1f;
 
     void Update()
     {
         if (isActive){
-            if (transform.position.y < -down){
+            if (transform.localPosition.y < down) {
                 direction = 1;
                 Debug.Log("is moving down!");
-            }else if (transform.position.y > up){
+            }else if (transform.localPosition.y > up){
                 direction = -1;
                 Debug.Log("is moving up!");
             }
             Debug.Log("is moving!");
-            transform.position += Vector3.up * direction * Time.deltaTime;
+            
+            transform.localPosition += Vector3.up * direction * Time.deltaTime;
         }
     }
 
