a1ed150aec557622ea3ec491f6dd9c30859d49b8
javi <zawer>
2023-07-30 12:51:44 +0200

    add player animations

--- Assets/scripts/Puzzle Scripts/player.cs
+++ Assets/scripts/Puzzle Scripts/player.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class player : MonoBehaviour
 {
+    const string IS_FALLING = "isFalling";
+    const string IS_MOVING = "isMoving";
+
     public GameObject spawnPoint;
+    private Animator? _animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         // Raycast to check if is a block in front of the player
-        
-       /*  if (!Physics.Raycast(transform.position + transform.forward, Vector3.down, 1)){
-            Debug.Log("There is nothing in DOWN / FRONT of the object!");
-        }
-        if (!Physics.Raycast(transform.position - transform.forward, Vector3.down, 1)){
-            Debug.Log("There is nothing in DOWN / BACK of the object!");
-        } */
-        
+
+        /*  if (!Physics.Raycast(transform.position + transform.forward, Vector3.down, 1)){
+             Debug.Log("There is nothing in DOWN / FRONT of the object!");
+         }
+         if (!Physics.Raycast(transform.position - transform.forward, Vector3.down, 1)){
+             Debug.Log("There is nothing in DOWN / BACK of the object!");
+         } */
+
+        _animator?.SetBool(IS_MOVING, false);
         if (transform.position.y < -0.01f){
             if (transform.position.y < -10){
+                _animator?.SetBool(IS_FALLING, false);
                 transform.position = spawnPoint.transform.position;
                 transform.rotation = Quaternion.identity;
             }
@@ -44,20 +52,28 @@ public class player : MonoBehaviour
         RaycastHit hitWall;
         RaycastHit hitEmpty;
 
+        _animator?.SetBool(IS_MOVING, true);
+
         if (Physics.Raycast(transform.position + transform.up, transform.forward * direction, out hitWall, 1) && (Physics.Raycast(transform.position, transform.forward * direction, out hitMove, 1))){
             Debug.Log("is hitting a wall!");
             if (hitWall.collider.gameObject.tag == "Portal"){
                 Debug.Log("is hitting a portal!");
                 transform.position = hitWall.collider.gameObject.GetComponent<portal>().spawnPoint.transform.position;
             }
-        }else if (Physics.Raycast(transform.position, transform.forward * direction, out hitMove, 1)){
-            transform.position += transform.forward * direction; 
+        }else if (Physics.Raycast(transform.position, transform.forward * direction, out hitMove, 1))
+        {
+            var endPosition = transform.position + (transform.forward * direction);
+            transform.DOMove(endPosition, 1);
             transform.position += transform.up;
         }else if (!Physics.Raycast(transform.position + transform.forward * direction, Vector3.down, out hitEmpty, 1)){
             Debug.Log("is falling!");
-            transform.position += transform.forward * direction;
-        }else{
-            transform.position += transform.forward * direction;
+            _animator?.SetBool(IS_FALLING, true);
+            var endPosition = transform.position + (transform.forward * direction);
+            transform.DOMove(endPosition, 1);
+        }
+        else{
+            var endPosition = transform.position + (transform.forward * direction);
+            transform.DOMove(endPosition, 1);
         }
     }
 
